9a0a51571254b9c12d805af9c946a675ca92112f
kiejibi <kaekie2004@gmail.com>
2023-12-01 15:49:22 +0700

    lab6_1.cpp completed

---
+++ lab6_6.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x;
+    cout << "Enter an integer: ";
+    cin >> x;
+    int even =  0, odd = 0;
+    while(x != 0){
+        if(x%2 == 0){
+            even++;
+        }else{ 
+            odd++;
+        }
+        cout << "Enter an integer:";
+        cin >> x;
+    }        
+    cout << "#Even numbers = " << even << "\n";
+    cout << "#Odd numbers = " << odd << "\n";
+    return 0;
+}
